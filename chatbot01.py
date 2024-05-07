9fdab073faee60906ca3d984bb514e6fa4377cab
Pratiyush Pathak <56939096+pratiyushpathak@users.noreply.github.com>
2024-05-07 11:07:38 +0530

    Update chatbot01.py

--- chatbot01.py
+++ chatbot01.py
@@ -7,7 +7,7 @@ import vectordb
 def get_conversational_chain():
     prompt_template = """
     Answer the question as detailed as possible from the provided context, make sure to provide all the details,if the question is about you or a greeting then greet the user as well or if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    provided context just say, "answer is not available in the context", don't provide the wrong answer. If the question is numeric the give the correct solution\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
 
